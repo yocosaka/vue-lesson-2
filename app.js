09c604903e9e0a750aa0a9d636811e85be504a1b
yocosaka <sakayoco55@gmail.com>
2022-02-08 09:07:06 +0900

    Use dynamic classes using bind and object with key value pairs

--- app.js
+++ app.js
@@ -4,9 +4,24 @@ const app = Vue.createApp({
       url: 'https://apricot-web.com',
       showBooks: true,
       books: [
-        { title: 'Book1', author: 'Yoko', image: './assets/1.jpg' },
-        { title: 'Book2', author: 'Vicky', image: './assets/2.jpg' },
-        { title: 'Book3', author: 'Asahi', image: './assets/3.jpg' },
+        {
+          title: 'Book1',
+          author: 'Yoko',
+          image: './assets/1.jpg',
+          isFav: true,
+        },
+        {
+          title: 'Book2',
+          author: 'Vicky',
+          image: './assets/2.jpg',
+          isFav: false,
+        },
+        {
+          title: 'Book3',
+          author: 'Asahi',
+          image: './assets/3.jpg',
+          isFav: true,
+        },
       ],
     };
   },
