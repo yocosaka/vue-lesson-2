0f4b223c968a9786a95eb937f1e843479fbfd8ba
yocosaka <sakayoco55@gmail.com>
2022-02-08 14:02:38 +0900

    Add Modal component and display it in App, Creat global.css and import it in main.js

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="myInput" />
-  <button @click="handleClick">Click me</button>
+  <Modal />
 </template>
 
 <script>
+import Modal from './components/Modal';
+
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
       title: 'My first Vue app',
@@ -23,14 +25,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 
 h1 {
   border-bottom: 1px solid #ddd;
