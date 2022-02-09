7d164fdedf4d10f07deaa02d7aa2048fbf6501a4
yocosaka <sakayoco55@gmail.com>
2022-02-09 17:33:18 +0900

    Addd Block components and pass props of delay to it

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="block">click me {{ dealy }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Block',
+  props: ['delay'],
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
