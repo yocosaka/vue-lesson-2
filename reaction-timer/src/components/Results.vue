e2673fcc52c5430c746dcc02b447b3e391fd4c15
yocosaka <sakayoco55@gmail.com>
2022-02-09 18:04:05 +0900

    Add rank to message based on score

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,12 +1,33 @@
 <template>
-  <p class="results">Score: {{ score }}</p>
+  <p class="results">Score: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
   name: 'Results',
   props: ['score'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja Fingers';
+    } else if (this.score < 400) {
+      this.rank = 'Rapid Reflexes';
+    } else {
+      this.rank = 'Snail pace...';
+    }
+  },
 };
 </script>
 
-<style></style>
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
