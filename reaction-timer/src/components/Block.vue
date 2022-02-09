5b39f39cec0efe77218e1baab13236f4ad339ea8
yocosaka <sakayoco55@gmail.com>
2022-02-09 17:43:31 +0900

    Add lifecycle methods, mounted, updated, and unmounted

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="block">click me {{ dealy }}</div>
+  <div v-if="showBlock" class="block">click me {{ dealy }}</div>
 </template>
 
 <script>
 export default {
   name: 'Block',
   props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    console.log('component mounted');
+    setTimeout(() => {
+      this.showBlock = true;
+      console.log(this.delay);
+    }, this.delay);
+  },
+  updated() {
+    console.log('component updated');
+  },
+  unmounted() {
+    console.log('component unmounted');
+  },
 };
 </script>
 
