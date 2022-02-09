d880948e3e0cc3e9bc7e6af8485f63b0fb24864b
yocosaka <sakayoco55@gmail.com>
2022-02-09 17:59:49 +0900

    Emmit Custom events with data in components to send data to parent and another component

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,25 +1,36 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="startGame" :disabled="isPlaying">play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @endGame="endGame" />
+  <!-- <p>Reaction time: {{ score }} ms</p> -->
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Block from './components/Block';
+import Results from './components/Results';
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResults: false,
     };
   },
   methods: {
     startGame() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
     },
   },
 };
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,12 @@
+<template>
+  <p class="results">Score: {{ score }}</p>
+</template>
+
+<script>
+export default {
+  name: 'Results',
+  props: ['score'],
+};
+</script>
+
+<style></style>
