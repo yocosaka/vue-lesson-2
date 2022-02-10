8e909f25f3d85679aa54b6f0bf9e6df70adf9958
yocosaka <sakayoco55@gmail.com>
2022-02-10 14:49:57 +0900

    Add simple Home and About views

--- ninja-jobs/src/views/Home.vue
+++ ninja-jobs/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut laborum quod
+      vero natus suscipit, inventore dolore, rerum ducimus minus facere quisquam
+      fuga similique aspernatur consequatur perferendis. Et totam repudiandae
+      porro!
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
