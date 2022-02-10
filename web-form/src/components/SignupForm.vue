c52b234b2df6ef5b967817795a017f611434ad5f
yocosaka <sakayoco55@gmail.com>
2022-02-10 11:15:15 +0900

    Select with v-model

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -4,10 +4,17 @@
     <input type="email" required v-model="email" />
     <label>Password:</label>
     <input type="password" required v-model="password" />
+
+    <label>Role</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
   </form>
 
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
 </template>
 
 <script>
@@ -17,6 +24,7 @@ export default {
     return {
       email: '',
       password: '',
+      role: '',
     };
   },
 };
@@ -40,7 +48,8 @@ label {
   letter-spacing: 1px;
   font-weight: bold;
 }
-input {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
