be784b337cd996fd1e2ceefa389dad738d54eab3
yocosaka <sakayoco55@gmail.com>
2022-02-10 16:26:59 +0900

    Add 404 page by adding route setting path to /:catchAll(.*)

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Jobs from '../views/jobs/Jobs.vue';
 import JobDetails from '../views/jobs/JobDetails.vue';
+import NotFound from '../views/NotFound.vue';
 
 const routes = [
   {
@@ -30,6 +31,12 @@ const routes = [
   {
     path: '/all-jobs',
     redirect: '/jobs',
+  },
+  // catchAll 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
   },
 ];
 
