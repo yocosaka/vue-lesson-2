23a592d8d2208eda711b69770c641082b90fc17d
yocosaka <sakayoco55@gmail.com>
2022-02-10 16:10:38 +0900

    Set id as data property through this.$route.params

--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,11 +1,16 @@
 <template>
   <h1>Job Detail Page</h1>
-  <p>Job Id is {{ $route.params.id }}</p>
+  <p>Job Id is {{ id }}</p>
 </template>
 
 <script>
 export default {
   name: 'JobDetails',
+  data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
 };
 </script>
 
