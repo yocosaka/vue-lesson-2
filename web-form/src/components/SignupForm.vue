8d89928fef1373cbfc7da9baf586072c465953c9
yocosaka <sakayoco55@gmail.com>
2022-02-10 11:19:52 +0900

    Checkbox with v-model for true/false in a sigle checkbox

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -10,11 +10,17 @@
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label>Accept terms and conditions</label>
+    </div>
   </form>
 
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
+  <p>Terms: {{ terms }}</p>
 </template>
 
 <script>
@@ -25,6 +31,7 @@ export default {
       email: '',
       password: '',
       role: '',
+      terms: false,
     };
   },
 };
@@ -57,5 +64,12 @@ select {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type='checkbox'] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
