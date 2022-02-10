802e7145fb8a69f2519640506c4553f46cd827a7
yocosaka <sakayoco55@gmail.com>
2022-02-10 16:04:31 +0900

    Add Jobs View and Create JobDetails

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
+import Jobs from '../views/jobs/Jobs.vue';
 
 const routes = [
   {
@@ -12,10 +13,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About,
-    // // route level code-splitting
-    // // this generates a separate chunk (about.[hash].js) for this route
-    // // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs,
   },
 ];
 
---
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -0,0 +1,10 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'JobDetails',
+};
+</script>
+
+<style></style>
