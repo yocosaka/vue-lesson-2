125eba88e2fd93132a80c824d83a16168329fe5f
yocosaka <sakayoco55@gmail.com>
2022-02-14 15:22:42 +0900

    Add code to fetch data from db using async await js fetching

--- apricot-blog/src/views/Home.vue
+++ apricot-blog/src/views/Home.vue
@@ -1,35 +1,42 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts" v-if="showPosts" />
-    <button @click="showPosts = !showPosts">Toggle Posts</button>
-    <button @click="posts.pop()">Delete a post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" v-if="showPosts" />
+    </div>
+    <div v-else>loading...</div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import PostList from '../components/PostList.vue';
 
 export default {
   name: 'Home',
   components: { PostList },
   setup() {
-    const posts = ref([
-      {
-        title: 'welcome to the blog',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in imperdiet nisi. Vestibulum eu dui dictum, ornare est non, dignissim dui. Donec at ex nulla. Aenean sem tortor, bibendum ac accumsan vel, elementum vel turpis. Nam auctor egestas pulvinar. Aenean placerat finibus finibus. Praesent dictum velit at purus aliquam, sit amet mattis est molestie. Duis a nulla et mauris luctus feugiat nec cursus odio. Nam elementum vitae est eu porttitor. Nulla vestibulum gravida magna eu ultricies. Nunc posuere tincidunt pellentesque. Sed id condimentum nisl, ultricies viverra orci. Fusce sit amet massa ut nibh feugiat elementum ac id est.',
-        id: 1,
-      },
-      {
-        title: 'top 5 CSS tips',
-        body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet incidunt cumque eaque suscipit veniam praesentium iste eos itaque odit asperiores hic distinctio aut perspiciatis laboriosam esse reprehenderit, quibusdam expedita repellendus.',
-        id: 2,
-      },
-    ]);
+    const posts = ref([]);
+    const error = ref(null);
 
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts');
+        if (!data.ok) {
+          throw Error('no data available');
+        }
+        posts.value = await data.json();
+      } catch (err) {
+        error.value = err.message;
+        console.log(err.message);
+      }
+    };
+    onMounted(() => {
+      load();
+    });
     const showPosts = ref(true);
-    return { posts, showPosts };
+    return { posts, showPosts, error };
   },
 };
 </script>
