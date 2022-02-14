da9770a380b373f6f0026d1f9d32210a285b0bf1
yocosaka <sakayoco55@gmail.com>
2022-02-14 15:34:17 +0900

    Create getPosts reusable composition functions in composables

--- apricot-blog/src/views/Home.vue
+++ apricot-blog/src/views/Home.vue
@@ -3,40 +3,24 @@
     <h1>Home</h1>
     <div v-if="error">{{ error }}</div>
     <div v-if="posts.length">
-      <PostList :posts="posts" v-if="showPosts" />
+      <PostList :posts="posts" />
     </div>
     <div v-else>loading...</div>
   </div>
 </template>
 
 <script>
-import { onMounted, ref } from 'vue';
 import PostList from '../components/PostList.vue';
+import getPosts from '../composables/getPosts';
 
 export default {
   name: 'Home',
   components: { PostList },
   setup() {
-    const posts = ref([]);
-    const error = ref(null);
+    const { posts, error, load } = getPosts();
+    load();
 
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts');
-        if (!data.ok) {
-          throw Error('no data available');
-        }
-        posts.value = await data.json();
-      } catch (err) {
-        error.value = err.message;
-        console.log(err.message);
-      }
-    };
-    onMounted(() => {
-      load();
-    });
-    const showPosts = ref(true);
-    return { posts, showPosts, error };
+    return { posts, error };
   },
 };
 </script>
