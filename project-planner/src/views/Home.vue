de17dd805fad0cc42d803a307dc9f6148ab782fc
yocosaka <sakayoco55@gmail.com>
2022-02-14 09:06:00 +0900

    Fetch data and display it on Home

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="home">Home</div>
+
+  <div v-if="projects.length">
+    <div v-for="project in projects" :key="project.id">
+      <p>{{ project.title }}</p>
+      <input type="checkbox" :checked="project.complete" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
   components: {},
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log('Error: ', err.message));
+  },
 };
 </script>
