a76eb7b1a4faef897365de884d700885b058b618
yocosaka <sakayoco55@gmail.com>
2022-02-14 14:51:00 +0900

    Add snippet using computed and props inside setup

--- apricot-blog/src/components/SinglePost.vue
+++ apricot-blog/src/components/SinglePost.vue
@@ -1,12 +1,19 @@
 <template>
   <h1>{{ post.title }}</h1>
-  <p>{{ post.body }}</p>
+  <p>{{ snippet }}</p>
 </template>
 
 <script>
+import { computed } from '@vue/runtime-core';
 export default {
   name: 'SinglePost',
   props: ['post'],
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.substring(0, 100) + '...';
+    });
+    return { snippet };
+  },
 };
 </script>
 
--- apricot-blog/src/views/Home.vue
+++ apricot-blog/src/views/Home.vue
@@ -15,13 +15,13 @@ export default {
   setup() {
     const posts = ref([
       {
-        title: 'Welcome to my blog',
-        body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+        title: 'welcome to the blog',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in imperdiet nisi. Vestibulum eu dui dictum, ornare est non, dignissim dui. Donec at ex nulla. Aenean sem tortor, bibendum ac accumsan vel, elementum vel turpis. Nam auctor egestas pulvinar. Aenean placerat finibus finibus. Praesent dictum velit at purus aliquam, sit amet mattis est molestie. Duis a nulla et mauris luctus feugiat nec cursus odio. Nam elementum vitae est eu porttitor. Nulla vestibulum gravida magna eu ultricies. Nunc posuere tincidunt pellentesque. Sed id condimentum nisl, ultricies viverra orci. Fusce sit amet massa ut nibh feugiat elementum ac id est.',
         id: 1,
       },
       {
-        title: 'My Second Title',
-        body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+        title: 'top 5 CSS tips',
+        body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet incidunt cumque eaque suscipit veniam praesentium iste eos itaque odit asperiores hic distinctio aut perspiciatis laboriosam esse reprehenderit, quibusdam expedita repellendus.',
         id: 2,
       },
     ]);
