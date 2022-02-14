2e18197ae889e5d9b3d76f082d0c7fd7f6f7b365
yocosaka <sakayoco55@gmail.com>
2022-02-14 14:18:51 +0900

    Add example code of computed in composition API

--- apricot-blog/src/views/Home.vue
+++ apricot-blog/src/views/Home.vue
@@ -1,37 +1,21 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update ninja one</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <p>{{ name }}</p>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from 'vue';
+import { computed, ref } from 'vue';
 
 export default {
   name: 'Home',
   setup() {
-    const ninjaOne = ref({ name: 'mario', age: 30 });
-    const ninjaTwo = reactive({ name: 'luigi', age: 35 });
+    const name = computed(() => {
+      return 'Vicky';
+    });
 
-    // reactive cannot be used on primitive values
-    const nameOne = ref('mario'); // will be reactive
-    const nameTwo = reactive('luigi'); // will NOT be reactive
-
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40;
-    };
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45;
-    };
-
-    return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo };
+    return { name };
   },
 };
 </script>
