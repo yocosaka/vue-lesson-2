64ec50b68f188842529afb3ea8f14213f069c673
yocosaka <sakayoco55@gmail.com>
2022-02-14 13:45:34 +0900

    Add test code in Home.vue to test setup, created, and mounted timing

--- apricot-blog/src/views/Home.vue
+++ apricot-blog/src/views/Home.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="home">Home</div>
+  <p>{{ name }} is {{ age }}.</p>
+  <button @click="handleClick">Click me</button>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  components: {},
+  setup() {
+    let name = 'vicky';
+    let age = 20;
+
+    const handleClick = () => {
+      console.log('you clicked me');
+    };
+    return {
+      name,
+      age,
+      handleClick,
+    };
+  },
 };
 </script>
