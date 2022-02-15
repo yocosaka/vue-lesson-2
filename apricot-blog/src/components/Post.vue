2f856d4c3cd3a24f95c8e6ce2d75b09c31878085
yocosaka <sakayoco55@gmail.com>
2022-02-16 08:39:21 +0900

    Update styles

--- apricot-blog/src/components/Post.vue
+++ apricot-blog/src/components/Post.vue
@@ -1,9 +1,11 @@
 <template>
-  <router-link :to="{ name: 'SinglePost', params: { id: post.id } }">
-    <h1>{{ post.title }}</h1>
-  </router-link>
-  <p>{{ snippet }}</p>
-  <span v-for="tag in post.tags" :key="tag">#{{ tag }} </span>
+  <div class="post">
+    <router-link :to="{ name: 'SinglePost', params: { id: post.id } }">
+      <h3>{{ post.title }}</h3>
+    </router-link>
+    <p>{{ snippet }}</p>
+    <span v-for="tag in post.tags" :key="tag">#{{ tag }} </span>
+  </div>
 </template>
 
 <script>
@@ -20,4 +22,30 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.post {
+  margin: 0 40px 30px;
+  padding-bottom: 30px;
+  border-bottom: 1px dashed #e7e7e7;
+}
+.post h3 {
+  display: inline-block;
+  position: relative;
+  font-size: 26px;
+  color: white;
+  margin-bottom: 10px;
+  max-width: 400px;
+}
+.post h3::before {
+  content: '';
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: #ff8800;
+  position: absolute;
+  z-index: -1;
+  padding-right: 40px;
+  left: -30px;
+  transform: rotateZ(-1deg);
+}
+</style>
