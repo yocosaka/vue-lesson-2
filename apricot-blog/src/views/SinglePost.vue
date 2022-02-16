46e6bad826318bd47a9a69355f55cda7a6680150
yocosaka <sakayoco55@gmail.com>
2022-02-16 09:00:34 +0900

    use useRoute by importing it from vue-router and retrieve route.params.id instead of using props.id

--- apricot-blog/src/views/SinglePost.vue
+++ apricot-blog/src/views/SinglePost.vue
@@ -11,13 +11,17 @@
 <script>
 import getPost from '../composables/getPost';
 import Spinner from '../components/Spinner.vue';
+import { useRoute } from 'vue-router';
 
 export default {
   name: 'SinglePost',
   props: ['id'],
   components: { Spinner },
   setup(props) {
-    const { post, error, load } = getPost(props.id);
+    const route = useRoute();
+    // console.log(route);
+    // const { post, error, load } = getPost(props.id);
+    const { post, error, load } = getPost(route.params.id);
     load();
 
     return {
